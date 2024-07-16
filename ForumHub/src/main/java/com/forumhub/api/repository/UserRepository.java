16e63d1453fd7672544c87fbe1551990706b9ec4
igopereira1 <igopereira1@gmail.com>
2024-07-16 18:18:03 -0300

    feat: add UserRepository interface for user data access

---
+++ ForumHub/src/main/java/com/forumhub/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.forumhub.api.repository;
+
+import com.forumhub.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
