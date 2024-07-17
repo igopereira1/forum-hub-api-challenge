9f88a93941961e219b7873e686de19f3fbdc1498
igopereira1 <igopereira1@gmail.com>
2024-07-16 22:17:17 -0300

    feat: add TopicRepository

---
+++ ForumHub/src/main/java/com/forumhub/api/repository/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.forumhub.api.repository;
+
+import com.forumhub.api.model.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+}
