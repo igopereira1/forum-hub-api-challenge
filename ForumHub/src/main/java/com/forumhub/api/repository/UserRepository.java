99d63fa0c7a9c38773bba83236609d52e027c52c
igopereira1 <igopereira1@gmail.com>
2024-07-16 22:23:53 -0300

    feat: add CourseRepository

--- ForumHub/src/main/java/com/forumhub/api/repository/UserRepository.java
+++ ForumHub/src/main/java/com/forumhub/api/repository/UserRepository.java
@@ -3,7 +3,8 @@ package com.forumhub.api.repository;
 import com.forumhub.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByLogin(String login);
+    Optional<UserDetails> findByLogin(String login);
 }
