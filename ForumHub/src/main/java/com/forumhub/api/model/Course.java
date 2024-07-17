a964ff6b50141056657d8c9be22d9dfda7a97cd2
igopereira1 <igopereira1@gmail.com>
2024-07-16 22:20:15 -0300

    feat: create Course entity

---
+++ ForumHub/src/main/java/com/forumhub/api/model/Course.java
@@ -0,0 +1,24 @@
+package com.forumhub.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Course")
+@Table(name = "courses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+}
