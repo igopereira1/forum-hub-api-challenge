bdd43b5e00ab0d0edf56f26715be2a32b62c3ad5
igopereira1 <igopereira1@gmail.com>
2024-07-24 10:35:52 -0300

    refactor: AnswerService methods to use AnswerDetailsDTO and handle topic status

--- ForumHub/src/main/java/com/forumhub/api/service/AnswerService.java
+++ ForumHub/src/main/java/com/forumhub/api/service/AnswerService.java
@@ -1,15 +1,17 @@
 package com.forumhub.api.service;
 
 import com.forumhub.api.dto.answer.AnswerCreateDTO;
+import com.forumhub.api.dto.answer.AnswerDetailsDTO;
 import com.forumhub.api.dto.answer.AnswerUpdateDTO;
 import com.forumhub.api.model.Answer;
+import com.forumhub.api.model.Status;
 import com.forumhub.api.model.Topic;
 import com.forumhub.api.model.User;
 import com.forumhub.api.repository.AnswerRepository;
 import com.forumhub.api.repository.TopicRepository;
 import com.forumhub.api.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -32,34 +34,44 @@ public class AnswerService {
     }
 
     @Transactional
-    public Answer createAnswer(AnswerCreateDTO answerCreateDTO) {
+    public AnswerDetailsDTO createAnswer(AnswerCreateDTO answerCreateDTO) {
         Topic topic = topicRepository.findById(answerCreateDTO.topicId())
-                .orElseThrow(() -> new IllegalArgumentException("Topic not found with id: " + answerCreateDTO.topicId()));
-
+                .orElseThrow(() -> new EntityNotFoundException("Topic not found with id: " + answerCreateDTO.topicId()));
         User author = (User) getCurrentUser();
         Answer answer = new Answer(answerCreateDTO);
+
         answer.setTopic(topic);
         answer.setAuthor(author);
 
-        return answerRepository.save(answer);
+        if (topic.getStatus() == Status.NAO_RESPONDIDO) {
+            topic.setStatus(Status.RESPONDIDO);
+            topicRepository.save(topic);
+        }
+
+        answer = answerRepository.save(answer);
+
+        return new AnswerDetailsDTO(answer);
     }
 
     @Transactional(readOnly = true)
-    public Answer getAnswerById(Long id) {
-        return answerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Answer not found with id: " + id));
+    public AnswerDetailsDTO getAnswerById(Long id) {
+        Answer answer = answerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Answer not found with id: " + id));
+
+        return new AnswerDetailsDTO(answer);
     }
 
     @Transactional
-    public Answer updateAnswer(Long id, AnswerUpdateDTO answerUpdateDTO) {
+    public AnswerDetailsDTO updateAnswer(Long id, AnswerUpdateDTO answerUpdateDTO) {
         Answer answer = answerRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Answer not found with id: " + id));
+                .orElseThrow(() -> new EntityNotFoundException("Answer not found with id: " + id));
 
-        // Update answer properties
         answer.setMessage(answerUpdateDTO.message());
         answer.setSolution(answerUpdateDTO.solution());
 
-        return answerRepository.save(answer);
+        Answer updatedAnswer = answerRepository.save(answer);
+
+        return new AnswerDetailsDTO(updatedAnswer);
     }
 
     @Transactional
@@ -71,13 +83,18 @@ public class AnswerService {
     }
 
     @Transactional(readOnly = true)
-    public List<Answer> getAllAnswers() {
-        return answerRepository.findAll();
+    public List<AnswerDetailsDTO> getAllAnswers() {
+        List<Answer> answers = answerRepository.findAll();
+
+        return answers.stream()
+                .map(AnswerDetailsDTO::new)
+                .toList();
     }
 
     private UserDetails getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String login = authentication.getName();
+
         return userRepository.findByLogin(login)
                 .orElseThrow(() -> new IllegalArgumentException("User not found with username: " + login));
     }
